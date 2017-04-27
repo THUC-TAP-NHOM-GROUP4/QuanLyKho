0f626a77f5949241196c223d89dc00d9509cc85a
nguyenhienmta <nguyenhienmta2008@gmail.com>
2017-04-27 21:13:07 +0700

    sualaicsdl

--- TTN_QLKHO.sql
+++ TTN_QLKHO.sql
@@ -13,69 +13,71 @@ diachi nvarchar(50),
 dienthoai int,
 chucvu nvarchar(50)
 )
-create table DMKho
+
+
+create table Kho
 (
-ma_kho varchar(20) primary key,
-ten_kho nvarchar(50)
+ma varchar(20) primary key,
+ten nvarchar(50)
 )
 
 
-create table DMKhachHang
+create table KhachHang
 (
-ma_kh varchar(20) primary key,
-ten_kh nvarchar(50),
-diachi_kh nvarchar(50),
-dienthoai_kh varchar(20),
+ma varchar(20) primary key,
+ten nvarchar(50),
+diachi nvarchar(50),
+dienthoai varchar(20),
 Email varchar(50),
 )
 
-create table DMNhom
+create table Nhom
 (
-ma_nhom varchar(20) primary key,
-ten_nhom nvarchar(50)
+ma varchar(20) primary key,
+ten nvarchar(50)
 )
 
 
-create table DMHangHoa
+create table HangHoa
 (
-ma_hh varchar(20) primary key,
-ten_hh nvarchar(50),
-nhom_ma varchar(20),
-FOREIGN KEY(nhom_ma) REFERENCES DMNhom(ma_nhom)
-
+ma varchar(20) primary key,
+ten nvarchar(50),
+nhomma varchar(20),
+FOREIGN KEY(nhomma) REFERENCES Nhom(ma)
 )
 
 CREATE TABLE PhieuXuat
 (
-maso_px varchar(20) primary key,
-ngayCT date,
-kho_ma varchar(20),
-nguoi_nhan nvarchar(50),
+ma varchar(20) primary key,
+ngay date,
+khoma varchar(20),
+manguoinhan varchar(20),
 noidung text,
-kh_ma varchar(20),
-nv_ma varchar(20)
-FOREIGN KEY(kho_ma) REFERENCES DMKho(ma_kho),
-FOREIGN KEY(kh_ma) REFERENCES DMKhachHang(ma_kh),
-FOREIGN KEY(nv_ma) REFERENCES NhanVien(ma)
+khachhangma varchar(20),
+nhanvienma varchar(20)
+FOREIGN KEY(khoma) REFERENCES Kho(ma),
+FOREIGN KEY(khachhangma) REFERENCES KhachHang(ma),
+FOREIGN KEY(nhanvienma) REFERENCES NhanVien(ma)
 
 )
 
-create table ChiTietPX
+create table ChiTietPhieuXuat
 (
-px_ma varchar(20),
-hh_ma varchar(20) primary key,
-ma_kho varchar(20),
-so_luong int,
-don_gia float,
-FOREIGN KEY(px_ma) REFERENCES PhieuXuat(maso_px),
-FOREIGN KEY(hh_ma) REFERENCES DMHangHoa(ma_hh)
-
+ma varchar(20) primary key,
+phieuxuatma varchar(20),
+hanghoama varchar(20),
+khoma varchar(20),
+soluong int,
+dongia float,
+FOREIGN KEY(phieuxuatma) REFERENCES PhieuXuat(ma),
+FOREIGN KEY(hanghoama) REFERENCES HangHoa(ma),
+FOREIGN KEY(khoma) REFERENCES Kho(ma)
 )
 
-create table DMNhaCungCap
+create table NhaCungCap
 (
-ma_ncc varchar(20) primary key,
-ten_ncc nvarchar(50),
+ma varchar(20) primary key,
+ten nvarchar(50),
 diachi nvarchar(50),
 dienthoai varchar(20),
 masothue int,
@@ -85,33 +87,33 @@ email varchar(50)
 
 create table PhieuNhap
 (
-maso_pn varchar(20) primary key,
-ngayCT date,
-ncc_ma varchar(20),
-nguoi_giao nvarchar(50),
+ma varchar(20) primary key,
+ngay date,
+nhacungcapma varchar(20),
+manguoigiao varchar(20),
 noidung text,
-kho_ma varchar(20),
-nv_ma varchar(20)
-FOREIGN KEY(kho_ma) REFERENCES DMKho(ma_kho),
-FOREIGN KEY(ncc_ma) REFERENCES DMNhaCungCap(ma_ncc),
-FOREIGN KEY(nv_ma) REFERENCES NhanVien(ma)
+khoma varchar(20),
+nhanvienma varchar(20)
+FOREIGN KEY(khoma) REFERENCES Kho(ma),
+FOREIGN KEY(nhacungcapma) REFERENCES NhaCungCap(ma),
+FOREIGN KEY(nhanvienma) REFERENCES NhanVien(ma)
 
 )
 
 
 
-create table ChiTietPN
+create table ChiTietPhieuNhan
 (
-pn_soma varchar(20) ,
-hh_ma varchar(20),
-kho_ma varchar(20),
-so_luong int , 
-don_gia float,
-FOREIGN KEY(pn_soma) REFERENCES PhieuNhap(maso_pn),
-FOREIGN KEY(hh_ma) REFERENCES DMHangHoa(ma_hh)
-
+ma varchar(20) primary key ,
+phieunhanma varchar(20),
+hanghoama varchar(20),
+khoma varchar(20),
+soluong int , 
+dongia float,
+FOREIGN KEY(phieunhanma) REFERENCES PhieuNhap(ma),
+FOREIGN KEY(hanghoama) REFERENCES HangHoa(ma),
+FOREIGN KEY(khoma) REFERENCES Kho(ma)
 )
-
 
 
 
@@ -123,7 +125,7 @@ INSERT INTO NhanVien VALUES('NV001' ,N'Nguyễn Văn An', '04/05/1992', '0' , N'
 
 --insert bảng danh mục kho , mã kho , tên kho
 
-INSERT INTO DMKho VALUES('MK001',N'Kho chứa hàng thực phẩm'),
+INSERT INTO Kho VALUES('MK001',N'Kho chứa hàng thực phẩm'),
                         ('MK002',N'Kho chứa hàng xuất khẩu'),
 						('MK003',N'Kho chứa hàng tồn đọng'),
 						('MK004',N'Kho chứa hàng gia dụng'),
@@ -132,7 +134,7 @@ INSERT INTO DMKho VALUES('MK001',N'Kho chứa hàng thực phẩm'),
 --insert bảng danh mục khách hàng : makh, tenkh , diachi, dien thoai, email
 
 
-INSERT INTO DMKhachHang VALUES ('KH001',N'Nguyễn Văn An', N'Hà Nội','0123456789','nguyenan@gmail.com'),
+INSERT INTO KhachHang VALUES ('KH001',N'Nguyễn Văn An', N'Hà Nội','0123456789','nguyenan@gmail.com'),
                          ('KH002',N'Nguyễn Văn Hải', N'Hải phòng','0142367589','nguyenhai45@gmail.com'),
 						 ('KH003',N'Nguyễn Văn Nam', N'Hà Nội','0126797859','nguyennan@gmail.com'),
 						 ('KH004',N'Nguyễn Văn Bình', N'Hà giang','03985789','nguyenbinh@gmail.com'),
@@ -141,7 +143,7 @@ INSERT INTO DMKhachHang VALUES ('KH001',N'Nguyễn Văn An', N'Hà Nội','01234
               
 
 
-INSERT INTO DMNhom VALUES ('N001',N'nhóm hàng gia dụng '), 
+INSERT INTO Nhom VALUES ('N001',N'nhóm hàng gia dụng '), 
 						  ('N002',N'nhóm hàng thực phẩm '),   
 						  ('N003',N'nhóm hàng tồn kho '),  
 						  ('N004',N'nhóm hàng xuất khẩu'), 
@@ -150,7 +152,7 @@ INSERT INTO DMNhom VALUES ('N001',N'nhóm hàng gia dụng '),
 
 					
 
-INSERT INTO DMHangHoa VALUES ('HH001','gạo','N002'),
+INSERT INTO HangHoa VALUES ('HH001','gạo','N002'),
                               ('HH002','quần áo','N005'),
 							   ('HH003','Mì tôm','N002'),
 							    ('HH004','Bánh','N002'),
@@ -158,22 +160,22 @@ INSERT INTO DMHangHoa VALUES ('HH001','gạo','N002'),
 
 	
 
-INSERT INTO PhieuXuat VALUES ('PX001', '02/03/2015','MK001',N'Lê văn Nam' ,'nội dung nhận hàng' , 'KH001' ,'NV001'),
-                             ('PX002', '02/03/2015','MK002',N'Lê văn Nam' ,'nội dung nhận hàng' , 'KH002','NV002'),
-							 ('PX003', '02/03/2015','MK003',N'Lê văn Nam' ,'nội dung nhận hàng' , 'KH003','NV003'),
-							 ('PX004', '02/03/2015','MK004',N'Lê văn Nam' ,'nội dung nhận hàng' , 'KH004','NV004'),
-							 ('PX005', '02/03/2015','MK005',N'Lê văn Nam' ,'nội dung nhận hàng' , 'KH005','NV005')
+INSERT INTO PhieuXuat VALUES ('PX001', '02/03/2015','MK001','NN001' ,'nội dung nhận hàng' , 'KH001' ,'NV001'),
+                             ('PX002', '02/03/2015','MK002','NN002' ,'nội dung nhận hàng' , 'KH002','NV002'),
+							 ('PX003', '02/03/2015','MK003','NN003' ,'nội dung nhận hàng' , 'KH003','NV003'),
+							 ('PX004', '02/03/2015','MK004','NN004' ,'nội dung nhận hàng' , 'KH004','NV004'),
+							 ('PX005', '02/03/2015','MK005','NN005' ,'nội dung nhận hàng' , 'KH005','NV005')
 
 
 
-INSERT INTO ChiTietPX VALUES ('PX001', 'HH001' , 'MK001' , '200' , '20.000'),
-                             ('PX002', 'HH002' , 'MK002' , '200' , '20.000'),
-							 ('PX003', 'HH003 ' , 'MK003' , '200' , '20.000'),
-							 ('PX004', 'HH004' , 'MK004' , '200' , '20.000'),
-							 ('PX005', 'HH005' , 'MK005' , '200' , '20.000')
+INSERT INTO ChiTietPhieuXuat VALUES ('CTPX001','PX001', 'HH001' , 'MK001' , '200' , '20.000'),
+                             ('CTPX002','PX002', 'HH002' , 'MK002' , '200' , '20.000'),
+							 ('CTPX003','PX003', 'HH003 ' , 'MK003' , '200' , '20.000'),
+							 ('CTPX004','PX004', 'HH004' , 'MK004' , '200' , '20.000'),
+							 ('CTPX005','PX005', 'HH005' , 'MK005' , '200' , '20.000')
 
 
-INSERT INTO DMNhaCungCap VALUES ('NCC01' , N'Thực phẩm sạch HN' , N'Hà Nội' ,'0123546987', '0001' , 'thucphamHN@gmail.com'),
+INSERT INTO NhaCungCap VALUES ('NCC01' , N'Thực phẩm sạch HN' , N'Hà Nội' ,'0123546987', '0001' , 'thucphamHN@gmail.com'),
                                 ('NCC02' , N'Hàng Gia Dụng HP' , N'Hải Phòng' ,'0123546987', '0001' , 'hanggiadungHP@gmail.com'),
 								('NCC03' , N'Nhà May Việt Tín HN' , N'Hà Nội' ,'0123546987', '0001' , 'viettinHN@gmail.com'),
 								('NCC04' , N'Tường AN' , N'Hà Nam' ,'0123546987', '0001' , 'tuonganHN@gmail.com'),
@@ -191,8 +193,8 @@ INSERT INTO PhieuNhap VALUES ('PN001', '04/05/2015', 'NCC01' , N'Nguyễn Văn N
 
 
 
-INSERT INTO ChiTietPN VALUES ('PN001' , 'HH001' , 'MK001' , '400' ,'25.000000'),
-                             ('PN002' , 'HH002' , 'MK002' , '400' ,'25.000000'),
-							 ('PN003' , 'HH003' , 'MK003' , '400' ,'25.000000'),
-							 ('PN004' , 'HH004' , 'MK004' , '400' ,'25.000000'),
-							 ('PN005' , 'HH005' , 'MK005' , '400' ,'25.000000')
+INSERT INTO ChiTietPhieuNhan VALUES ('CTPN001','PN001' , 'HH001' , 'MK001' , '400' ,'25.000000'),
+                             ('CTPN002','PN002' , 'HH002' , 'MK002' , '400' ,'25.000000'),
+							 ('CTPN003','PN003' , 'HH003' , 'MK003' , '400' ,'25.000000'),
+							 ('CTPN004','PN004' , 'HH004' , 'MK004' , '400' ,'25.000000'),
+							 ('CTPN005','PN005' , 'HH005' , 'MK005' , '400' ,'25.000000')
